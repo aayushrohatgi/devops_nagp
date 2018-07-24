2cecc8a39c1966198b3f3d21583a85c2912a7bd0
Aayush Rohatgi <aayushrohatgi07@gmail.com>
2018-07-24 14:16:43 +0530

    sonar changes

--- src/main/java/com/devopssample/controller/FrontController.java
+++ src/main/java/com/devopssample/controller/FrontController.java
@@ -7,17 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/*
+*
+* class
+*/
 @Controller
 @RequestMapping(value = "/")
 public class FrontController{
-	
+
+	/*
+	 *
+	 * method
+	 */
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ModelAndView getHomeView(HttpServletRequest request) {
-		StringBuilder str=new StringBuilder("test");
-		str.append("string");
-		System.out.println(str.toString() + " got it");
-		ModelAndView model = new ModelAndView("home");
-		return model;
+		int a=10,c=5;
+		c = a+c;
+		return new ModelAndView("home");
 	}
 
 }
